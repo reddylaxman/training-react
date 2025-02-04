d4d6126103acb6e334ece5072aa40b0d123c7020
reddylaxman <asl.my.dream@gmail.com>
2025-02-04 19:31:09 +0530

    React app

---
+++ src/components/Sidebar1.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Sidebar1 = () => {
+  return <div className="sidebar1">Sidebar1</div>;
+};
---
+++ src/components/Sidebar2.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Sidebar2 = () => {
+  return <div className="sidebar2">Sidebar2</div>;
+};
